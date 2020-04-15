4e646c950687100464f0343cad36ff5101cdc202
Jawaher Al Harthi <38451511+joj19968@users.noreply.github.com>
2020-04-15 18:54:48 +0300

    Add files via upload

---
+++ js/secret.js
@@ -0,0 +1,7 @@
+// js/secret.js
+class SECRET {
+  static get mapbox_key() {
+    // return your mapbox API key
+    return "pk.eyJ1Ijoiam9qOTY5NyIsImEiOiJjazZqdnhvYXMwMGd5M21ubWpycms4eXhuIn0.m7HnzGtVJdMVjAmNou4pgQ";
+  }
+}
